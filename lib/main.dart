87cda31a5a97ea73722f560bfcd181512485f7bd
ChybaAdam <atymosz.contact@gmail.com>
2020-10-11 18:36:28 +0200

    change title of the application

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WordFishing!',
       theme: ApplicationThemes.getTheme(Themes.LIGHT),
       home: Scaffold(
         appBar: AppBar(
