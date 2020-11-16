56dc1fdbcffef4393c86094053168a5f9bc8d647
ChybaAdam <atymosz.contact@gmail.com>
2020-11-16 18:13:24 +0100

    fix animated drawer

--- lib/utils/typography.dart
+++ lib/utils/typography.dart
@@ -1,2 +1,20 @@
-const List<int> fontSize = const [100, 200, 300, 400, 500, 600, 700];
+const List<int> fontWeight = const [300, 700];
 const List<double> fontScale = const [1, 1.2, 1.4, 1.6, 1.8, 2.0];
+const List<int> fontSize = const [
+  10,
+  12,
+  14,
+  16,
+  18,
+  20,
+  22,
+  24,
+  26,
+  28,
+  30,
+  32,
+  34,
+  36,
+  38,
+  40
+];
