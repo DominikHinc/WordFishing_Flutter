72203e0e4888258d74cb7bf099356de5f98b2b86
ChybaAdam <atymosz.contact@gmail.com>
2020-12-16 16:26:06 +0100

    remove CustomListScreen from the drawer

--- lib/navigation/drawer-routes-config.dart
+++ lib/navigation/drawer-routes-config.dart
@@ -2,18 +2,18 @@ import 'package:WordFishing/screens/achievements-screen.dart';
 import 'package:WordFishing/screens/auth-screen.dart';
 import 'package:WordFishing/screens/book-screen.dart';
 import 'package:WordFishing/screens/credits-screen.dart';
-import 'package:WordFishing/screens/custom-list-screen.dart';
+// import 'package:WordFishing/screens/custom-list-screen.dart';
 import 'package:WordFishing/screens/debug-screen.dart';
 import 'package:WordFishing/screens/settings-screen.dart';
 import 'package:flutter/material.dart';
 
 Map<String, Widget Function(BuildContext)> getDrawerRouteNames() {
   return {
-    AchievementsScreen.routeName: (ctx) => AchievementsScreen(),
     BookScreen.routeName: (ctx) => BookScreen(),
     AuthScreen.routeName: (ctx) => AuthScreen(),
     CreditsScreen.routeName: (ctx) => CreditsScreen(),
-    CustomListScreen.routeName: (ctx) => CustomListScreen(),
+    AchievementsScreen.routeName: (ctx) => AchievementsScreen(),
+    // CustomListScreen.routeName: (ctx) => CustomListScreen(),
     SettingsScreen.routeName: (ctx) => SettingsScreen(),
     DebugScreen.routeName: (ctx) => DebugScreen(),
   };
