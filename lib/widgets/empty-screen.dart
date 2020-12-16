de7157e978dd70f4ec29578385793397cd80681f
ChybaAdam <atymosz.contact@gmail.com>
2020-12-16 17:10:15 +0100

    add a service currently unavailable message to the widget

--- lib/widgets/empty-screen.dart
+++ lib/widgets/empty-screen.dart
@@ -1,10 +1,41 @@
+import 'dart:async';
+
+import 'package:WordFishing/utils/spacing.dart';
 import 'package:flutter/material.dart';
 
-class EmptyScreen extends StatelessWidget {
+class EmptyScreen extends StatefulWidget {
   @override
+  _EmptyScreenState createState() => _EmptyScreenState();
+}
+
+class _EmptyScreenState extends State<EmptyScreen> {
+  Timer _timer;
+  Widget _emptyScreenChild = Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      CircularProgressIndicator(),
+      SizedBox(
+        height: spacing[3],
+      ),
+      Text("Loading..."),
+    ],
+  );
+  _EmptyScreenState() {
+    _timer = new Timer(const Duration(seconds: 15), () {
+      setState(() {
+        _emptyScreenChild = Text("Service currently unavailable");
+      });
+    });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Loading..."),
+      child: _emptyScreenChild,
     );
   }
 }
